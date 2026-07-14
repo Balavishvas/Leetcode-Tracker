015b89da50a1b0336b27647be81fb74cb0c0e7f0
Balavishvas <156605263+Balavishvas@users.noreply.github.com>
2026-07-14 14:16:03 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/561-ArrayPartition.py
@@ -0,0 +1,9 @@
+# Last updated: 7/14/2026, 2:16:03 PM
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        total=0
+        for i in range(0,len(nums),2):
+            total+=nums[i]
+        
+        return total
