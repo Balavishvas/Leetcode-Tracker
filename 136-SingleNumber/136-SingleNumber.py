181e24e62659676c5b3c2579368308cf91f604c4
Balavishvas <156605263+Balavishvas@users.noreply.github.com>
2026-07-14 14:16:43 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.py
@@ -0,0 +1,12 @@
+# Last updated: 7/14/2026, 2:16:43 PM
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                del d[i]
+        return list(d.keys())[0]
+        
