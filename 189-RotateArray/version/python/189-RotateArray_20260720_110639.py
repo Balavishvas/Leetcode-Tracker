b4336c30feb11f4267868df5e65f4bb95dc317e5
Balavishvas <156605263+Balavishvas@users.noreply.github.com>
2026-07-20 11:06:42 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/version/python/189-RotateArray_20260720_110639.py
@@ -0,0 +1,6 @@
+# Last updated: 7/20/2026, 11:06:39 AM
+1class Solution:
+2    def multiply(self, num1: str, num2: str) -> str:
+3        n1=int(num1)
+4        n2=int(num2)
+5        return str(n1*n2)
